efdc845e87795b1e698b1ab983bcb8859e193570
Saskia <bobinska.dev@gmail.com>
2025-08-29 13:56:06 +0200

    Set studioHost to 'structure-showroom' in Sanity CLI configuration

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -6,6 +6,7 @@ export default defineCliConfig({
     projectId,
     dataset: 'structure-showroom',
   },
+  studioHost: 'structure-showroom',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
