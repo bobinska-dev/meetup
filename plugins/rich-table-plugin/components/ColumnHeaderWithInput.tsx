af10064c14e91e168e591285eba91e3d4665893f
Saskia <bobinska.dev@gmail.com>
2026-01-30 19:38:22 +0100

    feat: enhance ColumnHeaderWithInput layout with improved styling and context menu integration

--- plugins/rich-table-plugin/components/ColumnHeaderWithInput.tsx
+++ plugins/rich-table-plugin/components/ColumnHeaderWithInput.tsx
@@ -1,7 +1,7 @@
 import { ColumnHeader } from '../schemas/columnHeader.object'
 import { ObjectItem, OperationsAPI } from 'sanity'
 import { ChangeEvent, ComponentType, useCallback, useState } from 'react'
-import { Card, TextInput } from '@sanity/ui'
+import { Box, Card, Flex, TextInput } from '@sanity/ui'
 
 import styled from 'styled-components'
 import ColumnContextMenu from './ColumnContextMenu'
@@ -52,47 +52,68 @@ export const ColumnHeaderWithInput: ComponentType<ColumnHeaderWithInputProps> = 
   // TODO ADD KEY WITH OPEN DIALOG TO FORCE REMOUNT
   const newColumnTitle = getLetterBasedOnIndex(columnIndex)
   return (
-    <StyledCard shadow={isFocused ? 1 : undefined} tone={isFocused ? 'primary' : undefined}>
-      <TextInput
-        onChange={handleChange}
-        onBlur={handleBlur}
-        onKeyDown={(e) => {
-          if (e.key === 'Enter') {
-            handleBlur()
-          }
-        }}
-        value={title}
-        aria-label="Column Header Title"
-        weight={'semibold'}
-        onFocus={() => setIsFocused(true)}
-        style={{
-          textAlign: 'center',
-          textOverflow: 'ellipsis',
-          color: 'var(--card-muted-fg-color)',
-        }}
-        title={title}
-        padding={0}
-        placeholder={newColumnTitle}
-        disabled={readOnly}
-        suffix={
-          <ColumnContextMenu
-            patch={patch}
-            path={path}
-            value={value}
-            columnHeaderKey={columnHeader._key}
-            columnIndex={columnIndex}
-            rowCount={rowCount}
-            columnCount={columnCount}
-            readOnly={readOnly}
+    <StyledCard
+      shadow={isFocused ? 1 : undefined}
+      tone={isFocused ? 'primary' : undefined}
+      paddingX={1}
+      paddingY={0}
+    >
+      <Flex justify={'space-between'} align={'center'} gap={1}>
+        <Box flex={1}>
+          <TextInput
+            onChange={handleChange}
+            onBlur={handleBlur}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                handleBlur()
+              }
+            }}
+            value={title}
+            aria-label="Column Header Title"
+            weight={'semibold'}
+            onFocus={() => setIsFocused(true)}
+            style={{
+              textAlign: 'center',
+              textOverflow: 'ellipsis',
+              color: 'var(--card-muted-fg-color)',
+              width: '100%',
+            }}
+            title={title}
+            padding={0}
+            placeholder={newColumnTitle}
+            disabled={readOnly}
+            /*          suffix={
+              <ColumnContextMenu
+                patch={patch}
+                path={path}
+                value={value}
+                columnHeaderKey={columnHeader._key}
+                columnIndex={columnIndex}
+                rowCount={rowCount}
+                columnCount={columnCount}
+                readOnly={readOnly}
+              />
+            }*/
           />
-        }
-      />
+        </Box>
+        <ColumnContextMenu
+          patch={patch}
+          path={path}
+          value={value}
+          columnHeaderKey={columnHeader._key}
+          columnIndex={columnIndex}
+          rowCount={rowCount}
+          columnCount={columnCount}
+          readOnly={readOnly}
+        />
+      </Flex>
     </StyledCard>
   )
 }
 const StyledCard = styled(Card)<{ $isFocused?: boolean }>`
   max-height: 50px;
-  border: unset;
+
+  //border: unset;
   [data-border] {
     box-shadow: unset;
   }
